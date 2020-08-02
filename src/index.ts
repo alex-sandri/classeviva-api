b5303f5e4b069dc687bdb9e5ca3ad005432579a4
Alex Sandri <me@alexsandri.com>
2020-08-02 14:16:04 +0200

    Access the current year's data

--- src/index.ts
+++ src/index.ts
@@ -60,7 +60,8 @@ export interface ClasseVivaDemerit
 
 export class ClasseViva
 {
-	private static YEAR = "19";
+	// TODO: Allow changing the year
+	private static YEAR = "";
 
 	private static ENDPOINTS = {
 		auth: () => `https://web${ClasseViva.YEAR}.spaggiari.eu/auth-p7/app/default/AuthApi4.php?a=aLoginPwd`,
