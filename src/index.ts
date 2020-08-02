9be84064ed3ef272ef56c5a00c970efd4e8661f4
Alex Sandri <me@alexsandri.com>
2020-08-02 10:48:18 +0200

    Add getGrades method

--- src/index.ts
+++ src/index.ts
@@ -8,11 +8,21 @@ export interface ClasseVivaProfile
 	school: string,
 }
 
+export interface ClasseVivaGrade
+{
+	subject: string,
+	grade: string,
+	type: string,
+	description: string,
+	date: string,
+};
+
 export class ClasseViva
 {
 	private static endpoints = {
 		auth: "https://web.spaggiari.eu/auth-p7/app/default/AuthApi4.php?a=aLoginPwd",
-		profile: "https://web.spaggiari.eu/home/app/default/menu_webinfoschool_studenti.php"
+		profile: "https://web.spaggiari.eu/home/app/default/menu_webinfoschool_studenti.php",
+		grades: "https://web.spaggiari.eu/cvv/app/default/genitori_note.php?filtro=tutto",
 	};
 
 	constructor(private sessionId: string)
@@ -28,6 +38,35 @@ export class ClasseViva
 			name: $(".name").text(),
 			school: $(".scuola").text(),
 		};
+	}
+
+	public async getGrades(): Promise<ClasseVivaGrade[]>
+	{
+		const response = await this.request(ClasseViva.endpoints.grades);
+
+		console.log(await response.text());
+
+		const $ = cheerio.load(await response.buffer());
+
+		const grades: ClasseVivaGrade[] = [];
+
+		$(".registro").each((i, element) =>
+		{
+			const subject = $(element).text();
+
+			$(element).parent().nextUntil("[align=center]").each(((i, grade) =>
+			{
+				grades.push({
+					subject,
+					grade: $(grade).find(".s_reg_testo").text(),
+					type: $(grade).find(".voto_data").last().text(),
+					description: $(grade).find("[colspan=32]").find("span").text(),
+					date: $(grade).find(".voto_data").first().text(),
+				});
+			}));
+		});
+
+		return grades;
 	}
 
 	public static async createSession(uid: string, pwd: string): Promise<ClasseViva>
